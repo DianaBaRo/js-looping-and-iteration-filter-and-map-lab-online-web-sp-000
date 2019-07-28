7806f584a1fccdd65559e597f66d9c2618b780f0
Diana Badas <diana.badas@gmail.com>
2019-07-28 21:29:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {return driver.revenue > revenue});
 }
 
-function driverNamesWithRevenueOver() {
-  
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (driver) {
+    if (driver.revenue > revenue) {
+      return driver.name
+    };
+  });
 }
